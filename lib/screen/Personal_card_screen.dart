0a6f2141dd60d7a1fbc75194612cf1495c8e6049
Danilo Santos <danilopsnts@gmail.com>
2023-02-04 10:26:48 -0300

    add AppBar

--- lib/screen/Personal_card_screen.dart
+++ lib/screen/Personal_card_screen.dart
@@ -8,6 +8,7 @@ class PersonalCardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(),
       body: SafeArea(
         child: Center(
           child: Text("data"),
