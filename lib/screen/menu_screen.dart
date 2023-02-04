98f2ee33e27d11febbbfeccbe49ae91ba19358f2
Danilo Santos <danilopsnts@gmail.com>
2023-02-04 14:21:32 -0300

    add RoundedButton

--- lib/screen/menu_screen.dart
+++ lib/screen/menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:aula1/screen/Dice_screen.dart';
 import 'package:aula1/screen/Personal_card_screen.dart';
+import 'package:aula1/widgets/Rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -22,42 +23,10 @@ class MenuScreen extends StatelessWidget {
             IntrinsicWidth(
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, PersonalCardScreen.id);
-                      },
-                      child: Material(
-                        color: Colors.teal,
-                        borderRadius: BorderRadius.circular(1000),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                          child: Text(
-                            'Cartão pessoal',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.ubuntu(color: Colors.white, fontSize: 16),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16, width: double.infinity),
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, DiceScreen.id);
-                      },
-                      child: Material(
-                        color: Colors.teal,
-                        borderRadius: BorderRadius.circular(1000),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                          child: Text(
-                            'Desafio dos dados',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.ubuntu(color: Colors.white, fontSize: 16),
-                          ),
-                        ),
-                      ),
-                    )
+                  children: const [
+                    RoundedButton(id:PersonalCardScreen.id, text: 'Cartão pessoal'),
+                    SizedBox(height: 16, width: double.infinity),
+                    RoundedButton(id: DiceScreen.id, text: 'Desafio dos dados'),
                   ]
               ),
             ),
---
+++ lib/widgets/Rounded_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RoundedButton extends StatelessWidget {
+
+  const RoundedButton({
+    super.key,
+    required this.id,
+    required this.text
+  });
+
+  final String id;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, id);
+      },
+      child: Material(
+        color: Colors.teal,
+        borderRadius: BorderRadius.circular(1000),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.ubuntu(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+}
