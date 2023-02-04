0011f4bbedacd043253969f1c313d25131f419eb
Danilo Santos <danilopsnts@gmail.com>
2023-02-04 09:55:50 -0300

    add screen

---
+++ lib/screen/menu_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Menu inicial',
+              style: TextStyle(fontSize: 26),
+            ),
+            SizedBox(
+                height: 16,
+                width: double.infinity),
+            Material(
+              color: Colors.teal,
+              borderRadius: BorderRadius.circular(1000),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: 16,
+                  horizontal: 32
+                ),
+                child: Text(
+                  'Cartão pessoal',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
