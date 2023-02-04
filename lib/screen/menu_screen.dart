377e572427a80d8407857b26919aead2cf0312d1
Danilo Santos <danilopsnts@gmail.com>
2023-02-04 09:59:34 -0300

    fix

--- lib/screen/menu_screen.dart
+++ lib/screen/menu_screen.dart
@@ -14,24 +14,16 @@ class MenuScreen extends StatelessWidget {
               'Menu inicial',
               style: TextStyle(fontSize: 26),
             ),
-            SizedBox(
-                height: 16,
-                width: double.infinity),
+            SizedBox(height: 16, width: double.infinity),
             Material(
               color: Colors.teal,
               borderRadius: BorderRadius.circular(1000),
               child: Padding(
-                padding: EdgeInsets.symmetric(
-                    vertical: 16,
-                  horizontal: 32
-                ),
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
                 child: Text(
                   'Cartão pessoal',
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16
-                  ),
+                  style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
               ),
             )
